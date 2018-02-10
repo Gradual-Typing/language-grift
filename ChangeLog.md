da5a9cf20341434bf5ef61bc7589d70c91481af2
Deyaaeldeen Almahallawi <dalmahal@indiana.edu>
2018-02-10 16:03:32 -0500

    init

---
+++ ChangeLog.md
@@ -0,0 +1,5 @@
+# Revision history for language-grift
+
+## 0.1.0.0  -- YYYY-mm-dd
+
+* First version. Released on an unsuspecting world.
---
+++ Language/Grift/Source/Parser/WithSourcePos.hs
@@ -0,0 +1,13 @@
+module Language.Grift.Source.Parser.WithSourcePos( TypeWithLoc
+                                          , ExpF1
+                                          , L1) where
+
+import           Text.Parsec.Pos (SourcePos)
+
+import           Language.Grift.Source.Syntax
+
+type WithLoc a = Ann SourcePos a
+
+type TypeWithLoc = WithLoc Type
+type ExpF1 = ExpF TypeWithLoc
+type L1 = WithLoc ExpF1
